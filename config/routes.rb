caa7a5e15bb7cf14346a02a4a86d32d8ee6a0bd8
H-kaichi02 <kaichi.hirate.1381@gmail.com>
2025-04-24 16:11:59 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
+  
+  # 修正: devise_forの引数にモデル名（:usersなど）を指定する
+  devise_for :users, modules: [:database_authenticatable, :registerable,
+                               :recoverable, :rememberable, :validatable, :confirmable]
 end
