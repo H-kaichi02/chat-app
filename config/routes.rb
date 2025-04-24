e44495a102f265f182f422db942dd33224fb6dda
H-kaichi02 <kaichi.hirate.1381@gmail.com>
2025-04-24 17:19:31 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
   
   # 修正: devise_forの引数にモデル名（:usersなど）を指定する
   devise_for :users, modules: [:database_authenticatable, :registerable,
