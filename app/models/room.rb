736782fcc12c49cbb301e7d50bab0650c21e5b4e
H-kaichi02 <kaichi.hirate.1381@gmail.com>
2025-04-25 14:47:38 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+  belongs_to :room_users
+  has_many :users, through: :room_users
 end
--- db/migrate/20250425051526_create_rooms.rb
+++ db/migrate/20250425051526_create_rooms.rb
@@ -1,4 +1,5 @@
 class CreateRooms < ActiveRecord::Migration[7.1]
+
   def change
     create_table :rooms do |t|
       t.string :name, null: false
