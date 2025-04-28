72625c34e6c576ef33ff33644a6a59e73a57bf34
H-kaichi02 <kaichi.hirate.1381@gmail.com>
2025-04-28 13:08:13 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'messages/index'
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   
   devise_for :users, modules: [:database_authenticatable, :registerable,
