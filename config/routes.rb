9e9ae27349f1a1e77799cf01cbe70090243271f2
H-kaichi02 <kaichi.hirate.1381@gmail.com>
2025-04-30 16:13:22 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
   
   devise_for :users, modules: [:database_authenticatable, :registerable,
                                :recoverable, :rememberable, :validatable, :confirmable]
