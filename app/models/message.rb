69b3d08526f3e456431f0b0edbb8d7c847d6ee9d
H-kaichi02 <kaichi.hirate.1381@gmail.com>
2025-04-30 16:57:43 +0900

    メッセージの表示

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,20 +2,4 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
-  def create
-    @tweet = Tweet.find(params[:tweet_id])
-    @comment = @tweet.comments.new(tweet_params)
-    if @message.save
-      redirect_to room_messages_path(@room)
-    else
-      render :index, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
-  end
-
 end
